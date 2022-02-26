8f57d1ec68fb6a035b7dbfb7ed961e20293de294
Lyké <95177582+LykeTheBest@users.noreply.github.com>
2022-02-26 13:46:43 +0300

    Update deleteCommand.js

--- src/functions/Funcs/calling/deleteCommand.js
+++ src/functions/Funcs/calling/deleteCommand.js
@@ -2,7 +2,7 @@ module.exports = async d => {
     const {code} = d.util.openFunc(d);
 
     d.message.delete().catch(e => {
-        d.aoiError.fnError(d, "custom", {}, "Failed To Delete Message With Reason: " + e);
+        d.aoiError.fnError(d, "custom", {}, "Mesaj silinirken bir hata ile karşılaşıldı: " + e);
     });
 
     return {
